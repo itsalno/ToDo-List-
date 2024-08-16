050a2325e253e17dc5b9f8ae6700747d52758533
alekss <anoviconoks@gmail.com>
2024-08-16 14:02:10 +0200

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import TodoList from "./TodoList.tsx";
+
+function App() {
+
+  return (
+    <>
+      <div className="app">
+          <TodoList/>
+      </div>
+    </>
+  )
+}
+
+export default App
